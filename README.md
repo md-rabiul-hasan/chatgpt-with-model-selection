4a776b6b3bef24e4fae56ac195fc947aad109cf2
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-12-28 12:10:27 +0600

    model integration

---
+++ README.md
@@ -0,0 +1,2 @@
+# ChatGPT application practice
+![Open AI CodeGPT](https://raw.githubusercontent.com/md-rabiul-hasan/chatgpt/main/ss.png)
--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
+import express from 'express'
+import cors from 'cors'
 import { Configuration, OpenAIApi } from "openai";
+import bodyParser from 'body-parser'
+
 const configuration = new Configuration({
     organization: "org-BkBV1Ulgpvlap1DCOeBu5ZbC",
     apiKey: "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",
@@ -19,3 +23,45 @@ async function callOpenAi(){
 }
 
 callOpenAi();
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from OpenAI!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  
+    const {message, currentModel} = req.body;
+    console.log(req.body)
+
+    const response = await openai.createCompletion({
+        model: `${currentModel}`,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0,
+    });
+    res.status(200).send({
+      message: response.data.choices[0].text
+    });
+
+})
+
+app.get('/models', async(req, res) => {
+  const response = await openai.listModels();
+  res.status(200).send({
+    models: response.data.data
+  });
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
