4f5aeaff4748d0bdc3acacfd22a5ba668fa2bc12
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2023-01-03 11:08:26 +0600

    updated api key

--- server/server.js
+++ server/server.js
@@ -5,24 +5,24 @@ import bodyParser from 'body-parser'
 
 const configuration = new Configuration({
     organization: "org-BkBV1Ulgpvlap1DCOeBu5ZbC",
-    apiKey: "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",
+    apiKey: "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",
 });
 
-// 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
+// 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
 
 const openai = new OpenAIApi(configuration);
 
-async function callOpenAi(){
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
-    });
-    console.log(response.data.choices[0].text);
-}
+// async function callOpenAi(){
+//     const response = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: "Say this is a test",
+//         max_tokens: 7,
+//         temperature: 0,
+//     });
+//     console.log(response.data.choices[0].text);
+// }
 
-callOpenAi();
+// callOpenAi();
 
 
 const app = express()
